d6ab21d148b4ffbc2266101bbce6f72e040b1858
Philipp Weber <Philipp.Weber@hs-augsburg.de>
2015-04-06 01:01:30 +0200

    Changes to be committed: 	modified:   data/linkfixer.js
    - Added MutationObserver to fix links when the website changes dynamically

--- data/linkfixer.js
+++ data/linkfixer.js
@@ -1,11 +1,31 @@
 //-----the my-subcriptions page-----
 //get all feed items
-var itemList = document.querySelectorAll(".feed-item-main-content");
-for (var i = 0; i < itemList.length; ++i) {
-  var clink = itemList[i].querySelector('a[href*="/channel/"]');
-  var ytid = clink.href.slice(clink.href.lastIndexOf("/")+1);
-  var linkList = itemList[i].querySelectorAll('a[href*="/watch?"]');
-  for(var j = 0; j < linkList.length; j++) {
-	linkList[j].href = linkList[j].href + "&channel=" + ytid;
-  }
+
+//static loading:
+callFixes();
+
+//dynamic loading
+var observer = new MutationObserver(function() {
+	callFixes();
+	console.log("chages observed");
+});
+observer.observe(document.body, {childList: true, subtree: false});
+
+
+function callFixes() {
+	if(location.href.search("/feed/") > -1)
+		fixSub();
+}
+
+function fixSub() {
+	var itemList = document.querySelectorAll(".feed-item-main-content");
+	for (var i = 0; i < itemList.length; ++i) {
+	  var clink = itemList[i].querySelector('a[href*="/channel/"]');
+	  var ytid = clink.href.slice(clink.href.lastIndexOf("/")+1);
+	  var linkList = itemList[i].querySelectorAll('a[href*="/watch?"]');
+	  for(var j = 0; j < linkList.length; j++) {
+		if(linkList[j].href.search("&channel=") == -1)
+			linkList[j].href = linkList[j].href + "&channel=" + ytid;
+	  }
+	}
 }
