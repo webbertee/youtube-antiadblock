9abf2215e645739feb7276fedc06647c2471274e
Philipp Weber <Philipp.Weber@hs-augsburg.de>
2015-04-06 13:02:29 +0200

    - fixing links in the sidebar automatically

--- data/linkfixer.js
+++ data/linkfixer.js
@@ -6,26 +6,45 @@ callFixes();
 
 //dynamic loading
 var observer = new MutationObserver(function() {
-	callFixes();
-	console.log("chages observed");
+    callFixes();
+    console.log("chages observed");
 });
 observer.observe(document.body, {childList: true, subtree: false});
 
 
 function callFixes() {
-	if(location.href.search("/feed/") > -1)
-		fixSub();
+    if(location.href.search("/feed/") > -1)
+        fixSub();
+    else if(location.href.search("/watch") > -1)
+        fixWatch();
 }
 
 function fixSub() {
-	var itemList = document.querySelectorAll(".feed-item-main-content");
-	for (var i = 0; i < itemList.length; ++i) {
-	  var clink = itemList[i].querySelector('a[href*="/channel/"]');
-	  var ytid = clink.href.slice(clink.href.lastIndexOf("/")+1);
-	  var linkList = itemList[i].querySelectorAll('a[href*="/watch?"]');
-	  for(var j = 0; j < linkList.length; j++) {
-		if(linkList[j].href.search("&channel=") == -1)
-			linkList[j].href = linkList[j].href + "&channel=" + ytid;
-	  }
-	}
+    var itemList = document.querySelectorAll(".feed-item-main-content");
+    for (var i = 0; i < itemList.length; ++i) {
+        //clink is the link to the channel
+        var clink = itemList[i].querySelector('a[href*="/channel/"]');
+        var ytid = clink.href.slice(clink.href.lastIndexOf("/")+1);
+        extendLinks(itemList[i], ytid);        
+    }
+}
+
+function fixWatch() {
+    var itemList = document.querySelectorAll(".video-list-item");
+    for (var i = 0; i < itemList.length; ++i) {
+        //hovercSpan belongs to the g+ hovercard
+        var hovercSpan = itemList[i].querySelector('.g-hovercard');
+        if(hovercSpan) {
+            var ytid = hovercSpan.getAttribute("data-ytid");
+            extendLinks(itemList[i], ytid);
+        }
+    }
+}
+
+function extendLinks(element, ytid) {
+    var linkList = element.querySelectorAll('a[href*="/watch?"]');
+    for(var j = 0; j < linkList.length; j++) {
+        if(linkList[j].href.search("&channel=") == -1)
+            linkList[j].href = linkList[j].href + "&channel=" + ytid;
+    }
 }
